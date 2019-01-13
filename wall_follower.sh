e0323e13286939cc49e4093fe1cc802db2d28fe5
Dmitry Gavrilenko <dmitry@gavrilenko>
2019-01-13 07:38:57 +0000

    Add wall_follower node

---
+++ wall_follower.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e " source ../../devel/setup.bash; rosrun wall_follower wall_follower" 
+
