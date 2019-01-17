85e4a9a23c4f9711b1d89b01014b6a4ad5cf0146
Dmitry Gavrilenko <dmitry@gavrilenko>
2019-01-17 17:46:12 +0000

    Add gmapping_demo.launch with default values

--- wall_follower.sh
+++ wall_follower.sh
@@ -2,7 +2,7 @@
 
 xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e " source ../../devel/setup.bash; roslaunch wall_follower gmapping_demo.launch" &
 sleep 5
 xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
