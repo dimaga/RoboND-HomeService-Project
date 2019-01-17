d4f94ee7f61ef0b1a405e5c920a6d4bb551c9ccd
Dmitry Gavrilenko <dmitry@gavrilenko>
2019-01-17 18:40:19 +0000

    Fix gmapping_demo.launch values

--- wall_follower.sh
+++ wall_follower.sh
@@ -2,9 +2,8 @@
 
 xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e " source ../../devel/setup.bash; roslaunch wall_follower gmapping_demo.launch" &
+xterm -e " source ../../devel/setup.bash; rosrun wall_follower wall_follower" &
 sleep 5
 xterm -e " source ../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e " source ../../devel/setup.bash; rosrun wall_follower wall_follower" 
-
+xterm -e " source ../../devel/setup.bash; roslaunch wall_follower gmapping_demo.launch" 
