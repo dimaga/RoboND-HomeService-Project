afa7e6c4802734fe6bc3d3d5f9128a7048f66b8c
Dmitry Gavrilenko <dmitry@gavrilenko>
2019-01-19 19:04:08 +0000

    Add add_markers.sh

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -2,12 +2,12 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
-// Define a client for to send zone1 requests to the move_base server through a SimpleActionClient
+// Define a client for to send pick_zone requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
-  // Initialize the simple_navigation_zone1s node
-  ros::init(argc, argv, "simple_navigation_zone1s");
+  // Initialize the simple_navigation_pick_zones node
+  ros::init(argc, argv, "simple_navigation_pick_zones");
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -17,41 +17,41 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  move_base_msgs::MoveBaseGoal zone1;
-  zone1.target_pose.header.frame_id = "map";
-  zone1.target_pose.header.stamp = ros::Time::now();
-  zone1.target_pose.pose.position.x = 1.0;
-  zone1.target_pose.pose.position.y = 3.0;
-  zone1.target_pose.pose.orientation.w = 1.0;
+  move_base_msgs::MoveBaseGoal pick_zone;
+  pick_zone.target_pose.header.frame_id = "map";
+  pick_zone.target_pose.header.stamp = ros::Time::now();
+  pick_zone.target_pose.pose.position.x = 1.0;
+  pick_zone.target_pose.pose.position.y = 3.0;
+  pick_zone.target_pose.pose.orientation.w = 1.0;
 
-  ROS_INFO("Sending pick up zone1");
-  ac.sendGoal(zone1);
+  ROS_INFO("Moving to pick up zone");
+  ac.sendGoal(pick_zone);
   ac.waitForResult();
 
-  // Check if the robot reached its zone1
+  // Check if the robot reached its pick_zone
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Succeeded reaching zone1");
+    ROS_INFO("Succeeded reaching pick up zone");
   else
-    ROS_INFO("Failed to reach zone1");
+    ROS_INFO("Failed to reach pick up zone");
 
   ros::Duration(5.0).sleep();
 
-  move_base_msgs::MoveBaseGoal zone2;
-  zone2.target_pose.header.frame_id = "map";
-  zone2.target_pose.header.stamp = ros::Time::now();
-  zone2.target_pose.pose.position.x = -3.0;
-  zone2.target_pose.pose.position.y = 3.0;
-  zone2.target_pose.pose.orientation.w = 1.0;
+  move_base_msgs::MoveBaseGoal drop_zone;
+  drop_zone.target_pose.header.frame_id = "map";
+  drop_zone.target_pose.header.stamp = ros::Time::now();
+  drop_zone.target_pose.pose.position.x = -3.0;
+  drop_zone.target_pose.pose.position.y = 3.0;
+  drop_zone.target_pose.pose.orientation.w = 1.0;
 
-  ROS_INFO("Sending pick up zone2");
-  ac.sendGoal(zone2);
+  ROS_INFO("Sending drop off zone");
+  ac.sendGoal(drop_zone);
   ac.waitForResult();
 
-  // Check if the robot reached zone2
+  // Check if the robot reached drop_zone
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Succeeded reaching zone2");
+    ROS_INFO("Succeeded reaching drop off zone");
   else
-    ROS_INFO("Failed to reach zone2");
+    ROS_INFO("Failed to reach drop off zone");
 
   return 0;
 }
